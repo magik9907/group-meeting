c578f949b7a0ea6e9ddf1582309ac1936b1f4abe
Kamil Milewski <kamilewski@yahoo.com>
2021-05-24 16:59:11 +0200

    Fix Register

--- GroupMeeting/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GroupMeeting/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -26,13 +26,13 @@ namespace GroupMeeting.Areas.Identity.Pages.Account
         private readonly SignInManager<User> _signInManager;
         private readonly UserManager<User> _userManager;
         private readonly ILogger<RegisterModel> _logger;
-        private readonly EmailSender _emailSender;
+        private readonly IEmailSender _emailSender;
 
         public RegisterModel(
             UserManager<User> userManager,
             SignInManager<User> signInManager,
             ILogger<RegisterModel> logger,
-            EmailSender emailSender)
+            IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -105,7 +105,7 @@ namespace GroupMeeting.Areas.Identity.Pages.Account
                         protocol: Request.Scheme);
 
                     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        "Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                        "Please confirm your account by <a href=" + HtmlEncoder.Default.Encode(callbackUrl) + ">clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
