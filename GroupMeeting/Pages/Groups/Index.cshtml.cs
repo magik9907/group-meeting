bf93ed1523638cdccc57ea3e405e7b65538f4805
Łukasz <43967017+magik9907@users.noreply.github.com>
2021-05-26 15:40:04 +0200

    searching group

--- GroupMeeting/Pages/Groups/Index.cshtml.cs
+++ GroupMeeting/Pages/Groups/Index.cshtml.cs
@@ -7,24 +7,41 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using GroupMeeting.Data;
 using GroupMeeting.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace GroupMeeting
 {
     public class IndexModel : PageModel
     {
+        public class SearchGroup
+        {
+            [Required]
+            public string Name { get; set; }
+        }
+
         private readonly GroupMeeting.Data.GroupMeetingContext _context;
+        [BindProperty]
+        public SearchGroup GroupName { get; set; }
 
         public IndexModel(GroupMeeting.Data.GroupMeetingContext context)
         {
             _context = context;
         }
 
-        public IList<Group> Group { get;set; }
+        public IList<Group> Group { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string? name)
         {
-            Group = await _context.Groups
-                .Include(a => a.Owner).ToListAsync();
+            if (name == null)
+                Group = await _context.Groups
+                    .Include(a => a.Owner).ToListAsync();
+            else
+                Group = await _context.Groups.Where(e =>e.Name == name).ToListAsync();
+        }
+
+        public IActionResult OnPostSearch()
+        {
+            return Redirect(String.Concat("/Groups?name=", GroupName.Name));
         }
     }
 }
