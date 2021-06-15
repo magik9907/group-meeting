40fad74f478b8a9f90477908c04ccf1653aa1700
Łukasz <43967017+magik9907@users.noreply.github.com>
2021-06-15 14:18:28 +0200

    Login fix

--- GroupMeeting/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ GroupMeeting/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,7 +81,7 @@ namespace GroupMeeting.Areas.Identity.Pages.Account
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(
-                    (await _userManager.FindByEmailAsync(Input.Email)),
+                    Input.Email,
                     Input.Password, Input.RememberMe,
                     lockoutOnFailure: false
                  );
